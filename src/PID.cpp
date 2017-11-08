75b2f07b3b44046d2c418ad0d9017fc76b11d0c0
Sameer Patil <sameerpatil38@gmail.com>
2017-11-07 19:44:49 -0800

    Adding PID code

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,26 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    p_error = 0;
+    i_error = 0;
+    d_error = 0;
+
+    PID::Kp = Kp;
+    PID::Ki = Ki;
+    PID::Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error = i_error + p_error;
+
 }
 
 double PID::TotalError() {
+    double steer_correction;
+
+    steer_correction = -Kp * p_error - Ki * i_error - Kd * d_error;
+    return steer_correction;
 }
 
